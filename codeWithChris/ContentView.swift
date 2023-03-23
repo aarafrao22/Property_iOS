97a4d042be81fe3e8f231e09777dd4eed8361eba
apple <aarafrao22@gmail.com>
2023-03-23 12:31:15 +0500

    InitialCommit

--- codeWithChris/ContentView.swift
+++ codeWithChris/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            
+            Color(.black).ignoresSafeArea() 
+            
+            VStack{
+                Image("aaraf")
+                    .resizable()
+                    .cornerRadius(45)
+                    .aspectRatio(contentMode: .fit).padding(14)
+                
+                Text("AARAF").font(.largeTitle).fontWeight(.bold).foregroundColor(Color.white)
+            }
         }
-        .padding()
     }
 }
 
