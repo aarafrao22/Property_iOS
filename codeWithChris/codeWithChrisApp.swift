ab92045c23fee67f404fae17d0f3a6411c1dd9a1
apple <aarafrao22@gmail.com>
2023-03-23 11:56:25 +0500

    Initial Commit

---
+++ codeWithChris/codeWithChrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeWithChrisApp.swift
+//  codeWithChris
+//
+//  Created by Rao Aaraf Younus on 2023-03-23.
+//
+
+import SwiftUI
+
+@main
+struct codeWithChrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
